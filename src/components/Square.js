5599c077852aad5ae8895a6c09e9a2634f12dc5d
Nata19754 <chavesnatally0419@gmail.com>
2023-01-03 10:46:13 -0800

    As a user, when I click on one of the question marks an alert appears with the index position of that question mark in the array.

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,10 @@
 import React from "react"
 
-const Square = ({ Square }) => {
+const Square = ({ Square, index, handleGameplay }) => {
+
+  const handleClick = () => {
+    
+  }
   return (
     <>
       <div className="square">{Square}</div>
